8fc8d4cee0a3ae18355a25f698bb63053dca4648
LmaAl-Johare1 <lmaref.j@gmail.com>
2024-07-23 23:35:54 +0300

    logger singelton app commit

---
+++ LoggerApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LoggerLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger = Logger.Instance;
+
+        logger.Log("Application started.");
+        logger.Log("Performing some operations...");
+        logger.Log("Application ended.");
+
+        Console.WriteLine("Messages have been logged.");
+    }
+}
---
+++ LoggerLibrary/Logger.cs
@@ -0,0 +1,47 @@
+﻿using LoggerLibrary;
+using System;
+using System.IO;
+
+namespace LoggerLibrary
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        private static readonly object _lock = new object();
+        private readonly string _logFilePath;
+
+        private Logger()
+        {
+            _logFilePath = "application.log";
+            Console.WriteLine("Logger initialized.");
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Logger();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            try
+            {
+                File.AppendAllText(_logFilePath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Logging error: {ex.Message}");
+            }
+        }
+    }
+}
+
